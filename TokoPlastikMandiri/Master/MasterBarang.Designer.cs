d18ce71b3e4c4301c2e2c6ba9d20ade5806fc27b
Vernnnz <ggulu846@gmail.com>
2021-11-23 11:19:02 +0700

    Tambah Master Barang

--- TokoPlastikMandiri/Master/MasterBarang.Designer.cs
+++ TokoPlastikMandiri/Master/MasterBarang.Designer.cs
@@ -29,12 +29,222 @@ namespace TokoPlastikMandiri.Master
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBoxInputBarang = new System.Windows.Forms.GroupBox();
+            this.tbKodeBarang = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNamaBarang = new System.Windows.Forms.TextBox();
+            this.cbKategoriBarang = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbHargaBeliBarang = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbHargaJualBarang = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbSatuanBarang = new System.Windows.Forms.ComboBox();
+            this.btnTambahMasterBarang = new System.Windows.Forms.Button();
+            this.btnUbahMasterBarang = new System.Windows.Forms.Button();
+            this.btnHapusMasterBarang = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBoxInputBarang.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxInputBarang
+            // 
+            this.groupBoxInputBarang.Controls.Add(this.cbSatuanBarang);
+            this.groupBoxInputBarang.Controls.Add(this.label6);
+            this.groupBoxInputBarang.Controls.Add(this.tbHargaJualBarang);
+            this.groupBoxInputBarang.Controls.Add(this.label5);
+            this.groupBoxInputBarang.Controls.Add(this.tbHargaBeliBarang);
+            this.groupBoxInputBarang.Controls.Add(this.label4);
+            this.groupBoxInputBarang.Controls.Add(this.label3);
+            this.groupBoxInputBarang.Controls.Add(this.cbKategoriBarang);
+            this.groupBoxInputBarang.Controls.Add(this.tbNamaBarang);
+            this.groupBoxInputBarang.Controls.Add(this.label2);
+            this.groupBoxInputBarang.Controls.Add(this.tbKodeBarang);
+            this.groupBoxInputBarang.Controls.Add(this.label1);
+            this.groupBoxInputBarang.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxInputBarang.Name = "groupBoxInputBarang";
+            this.groupBoxInputBarang.Size = new System.Drawing.Size(776, 171);
+            this.groupBoxInputBarang.TabIndex = 0;
+            this.groupBoxInputBarang.TabStop = false;
+            this.groupBoxInputBarang.Text = "Master Barang";
+            // 
+            // tbKodeBarang
+            // 
+            this.tbKodeBarang.BackColor = System.Drawing.SystemColors.Control;
+            this.tbKodeBarang.Enabled = false;
+            this.tbKodeBarang.Location = new System.Drawing.Point(96, 41);
+            this.tbKodeBarang.Name = "tbKodeBarang";
+            this.tbKodeBarang.Size = new System.Drawing.Size(230, 26);
+            this.tbKodeBarang.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kode";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama";
+            // 
+            // tbNamaBarang
+            // 
+            this.tbNamaBarang.Location = new System.Drawing.Point(96, 87);
+            this.tbNamaBarang.Name = "tbNamaBarang";
+            this.tbNamaBarang.Size = new System.Drawing.Size(230, 26);
+            this.tbNamaBarang.TabIndex = 3;
+            // 
+            // cbKategoriBarang
+            // 
+            this.cbKategoriBarang.FormattingEnabled = true;
+            this.cbKategoriBarang.Location = new System.Drawing.Point(353, 87);
+            this.cbKategoriBarang.Name = "cbKategoriBarang";
+            this.cbKategoriBarang.Size = new System.Drawing.Size(121, 28);
+            this.cbKategoriBarang.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kategori Barang ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Harga Beli";
+            // 
+            // tbHargaBeliBarang
+            // 
+            this.tbHargaBeliBarang.Location = new System.Drawing.Point(95, 132);
+            this.tbHargaBeliBarang.Name = "tbHargaBeliBarang";
+            this.tbHargaBeliBarang.Size = new System.Drawing.Size(231, 26);
+            this.tbHargaBeliBarang.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(349, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Harga Jual";
+            // 
+            // tbHargaJualBarang
+            // 
+            this.tbHargaJualBarang.Location = new System.Drawing.Point(484, 132);
+            this.tbHargaJualBarang.Name = "tbHargaJualBarang";
+            this.tbHargaJualBarang.Size = new System.Drawing.Size(230, 26);
+            this.tbHargaJualBarang.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(501, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Satuan Barang";
+            // 
+            // cbSatuanBarang
+            // 
+            this.cbSatuanBarang.FormattingEnabled = true;
+            this.cbSatuanBarang.Location = new System.Drawing.Point(505, 87);
+            this.cbSatuanBarang.Name = "cbSatuanBarang";
+            this.cbSatuanBarang.Size = new System.Drawing.Size(121, 28);
+            this.cbSatuanBarang.TabIndex = 11;
+            // 
+            // btnTambahMasterBarang
+            // 
+            this.btnTambahMasterBarang.Location = new System.Drawing.Point(13, 190);
+            this.btnTambahMasterBarang.Name = "btnTambahMasterBarang";
+            this.btnTambahMasterBarang.Size = new System.Drawing.Size(100, 35);
+            this.btnTambahMasterBarang.TabIndex = 1;
+            this.btnTambahMasterBarang.Text = "Tambah";
+            this.btnTambahMasterBarang.UseVisualStyleBackColor = true;
+            // 
+            // btnUbahMasterBarang
+            // 
+            this.btnUbahMasterBarang.Location = new System.Drawing.Point(119, 190);
+            this.btnUbahMasterBarang.Name = "btnUbahMasterBarang";
+            this.btnUbahMasterBarang.Size = new System.Drawing.Size(100, 35);
+            this.btnUbahMasterBarang.TabIndex = 2;
+            this.btnUbahMasterBarang.Text = "Ubah";
+            this.btnUbahMasterBarang.UseVisualStyleBackColor = true;
+            // 
+            // btnHapusMasterBarang
+            // 
+            this.btnHapusMasterBarang.Location = new System.Drawing.Point(225, 189);
+            this.btnHapusMasterBarang.Name = "btnHapusMasterBarang";
+            this.btnHapusMasterBarang.Size = new System.Drawing.Size(100, 35);
+            this.btnHapusMasterBarang.TabIndex = 3;
+            this.btnHapusMasterBarang.Text = "Hapus";
+            this.btnHapusMasterBarang.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 242);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 196);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // MasterBarang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnHapusMasterBarang);
+            this.Controls.Add(this.btnUbahMasterBarang);
+            this.Controls.Add(this.btnTambahMasterBarang);
+            this.Controls.Add(this.groupBoxInputBarang);
+            this.Name = "MasterBarang";
             this.Text = "MasterBarang";
+            this.groupBoxInputBarang.ResumeLayout(false);
+            this.groupBoxInputBarang.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxInputBarang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbKodeBarang;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNamaBarang;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbKategoriBarang;
+        private System.Windows.Forms.TextBox tbHargaJualBarang;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbHargaBeliBarang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbSatuanBarang;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTambahMasterBarang;
+        private System.Windows.Forms.Button btnUbahMasterBarang;
+        private System.Windows.Forms.Button btnHapusMasterBarang;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
